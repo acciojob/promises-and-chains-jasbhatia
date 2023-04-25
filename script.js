9082837fb0a53ddfaad7601ced8f288f0da079ff
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-25 09:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function validation(e){
+	e.preventDefault();
+	let age= document.getElementById("age").value;
+	let name= document.getElementById("name").value;
+	let promise= new Promise((resolve,reject)=>{
+		if(age>=18){
+			resolve(`Welcome,${name}. You can vote.`);
+		}
+		else{
+			reject(`Oh sorry ${name}. You aren't old enough.`);
+		}
+	})
+	 return promise;
+	
+	
+}
+
+validation().then((m)=>{
+	alert(m)
+},4000)
+.catch((e)=>{
+	alert(e)
+})
