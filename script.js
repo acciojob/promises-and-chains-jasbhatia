ec9520948af9e23360c81db567e852207bfb3351
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-25 16:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ function validation(){
 		alert("Please enter valid details");
 		return;
 	}
-	let obj={msg1:`Welcome,${name}. You can vote.`, msg2:`Oh sorry ${name}. You aren't old enough.`}
+	let obj={message:`Welcome,${name}. You can vote.`,showDiff:`Oh sorry ${name}. You aren't old enough.`}
 	let promise= new Promise((resolve,reject)=>{
 		if(age>=18){
-			resolve(obj.msg1)		}
+			resolve(obj.message)		}
 		else{
-			reject(obj.msg2);
+			reject(obj.showDiff);
 		}
 	})
 	 promise.then((msg)=>{
